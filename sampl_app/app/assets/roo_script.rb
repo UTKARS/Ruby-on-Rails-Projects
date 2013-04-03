c27347e55c25c6f2c61f83f851cd0b8b98808888
ainonext <sm_sv_ma@mail.ru>
2013-04-03 07:48:06 -0700

    for parsing xls

---
+++ sampl_app/app/assets/roo_script.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'roo'
+s=Roo::Excel.new("1it.xls")
+s.default_sheet = s.sheets.first
+#puts(s)
+3.upto(4) do |col|
+  group = s.cell(3,col)
+  puts(group)
+  if (group != nil) 
+
+    5.upto(40) do |line|
+      if(line==5)
+        puts "monday\n"
+      elsif(line==11)
+        puts "tuesday\n"
+      elsif(line == 17)
+        puts "wednesday\n"
+      elsif(line == 23)
+        puts "thursday\n"
+      elsif(line == 29)
+        puts "friday\n"
+      elsif(line == 35)
+        puts "saturday\n"
+      end
+      lesson = s.cell(line,col)
+      cabinet=s.cell(line,col+1)
+      class_num = (line - 5 + 6) % 6 + 1
+      puts "#{class_num}: #{lesson}: #{cabinet}"
+    end
+  end
+end
